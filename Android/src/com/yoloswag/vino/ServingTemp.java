b3fea2cfc4bd9c97f7b84e55b11cff1de464d7b0
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-04-30 23:31:32 -0700

    Dummy ServingTemperature class until Wilson pushes his

---
+++ Android/src/com/yoloswag/vino/ServingTemp.java
@@ -0,0 +1,13 @@
+package com.yoloswag.vino;
+
+/** Dummy class until Wilson uploads his
+ */
+public class ServingTemp 
+{
+	private String servingTemp;
+	
+	public ServingTemp(String servingTemp)
+	{
+		this.servingTemp = servingTemp;		
+	}
+}
