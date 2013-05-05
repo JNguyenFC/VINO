c0191f246e6363ba3d5306d479cafbde73ff7b37
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-05-05 14:20:40 -0700

    Changed format of fake database

--- Android/src/com/yoloswag/vino/Wine.java
+++ Android/src/com/yoloswag/vino/Wine.java
@@ -11,18 +11,14 @@ public class Wine
 	protected SweetOrDry sweetOrDry;
 	protected ServingTemp servingTemp;
 	
-	protected static Wine a = new Wine("Castello Banfi", "Italy", 2008, 12.9, "red", 
-	                                   "Chianti", "dry", "room temperature" );
-	protected static Wine b = new Wine("Barefoot", "California", 2009, 7.0, "white",
-	                                   "Moscato", "sweet", "chilled");
-	protected static Wine c = new Wine("Montana", "New Zealand", 2011, 13.4, "white",
-	                                   "Sauvignon Blanc", "dry", "chilled");
-	protected static Wine d = new Wine("Yellow Tail", "Australia", 2011, 14.8, "red",
-                                       "Cabernet Sauvignon", "dry", "room temperature");
-	protected static Wine e = new Wine("Chateau Ste. Michelle", "Florida", 2012, 11.2,
-                                       "white", "Riesling", "sweet", "chilled");
+	/**  Fake database of wines (to be deleted later)
+	 */
+	protected static Wine a = new Wine("Castello Banfi", "Italy", 2008, 12.9, "red", "Chianti", "dry", "room temperature" );
+	protected static Wine b = new Wine("Barefoot", "California", 2009, 7.0, "white", "Moscato", "sweet", "chilled");
+	protected static Wine c = new Wine("Montana", "New Zealand", 2011, 13.4, "white", "Sauvignon Blanc", "dry", "chilled");
+	protected static Wine d = new Wine("Yellow Tail", "Australia", 2011, 14.8, "red", "Cabernet Sauvignon", "dry", "room temperature");
+	protected static Wine e = new Wine("Chateau Ste. Michelle", "Florida", 2012, 11.2, "white", "Riesling", "sweet", "chilled");
 	
-
 	/**  Default constructor
 	 */
 	public Wine() 
@@ -45,9 +41,10 @@ public class Wine
 		this.servingTemp    = new ServingTemp(servingTemp);
 	}
 	
-	/**  Accessor for Entry
+	/**  Getter for Entry
 	 */
-	public static Wine get(Wine wine) {
+	public static Wine get(Wine wine) 
+	{
 		return wine;
 	}
 	
@@ -57,6 +54,7 @@ public class Wine
 	{	
 		return new Wine[] {a, b, c, d, e};
 	}
+	
 	
 	public Producer getProducer() 
 	{
