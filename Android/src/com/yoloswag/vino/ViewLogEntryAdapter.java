39d54c7ef8843ab61461042c160b62e22efb8c1d
rbcordov@ucsd.edu <rafaelcordova@Rafaels-MacBook-Pro-3.local>
2013-05-10 01:51:35 -0700

    Rafael: changed ViewLogEntryFragment, made ViewLogEntryAdapter. viewLogentryFragment should display an expandable list

---
+++ Android/src/com/yoloswag/vino/ViewLogEntryAdapter.java
@@ -0,0 +1,95 @@
+package com.yoloswag.vino;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ViewLogEntryAdapter extends BaseExpandableListAdapter {
+	private Context context;
+	private Entry[] entries;
+	
+	public ViewLogEntryAdapter(Context cont, Entry[] ent)
+	{
+		context = cont;
+		entries = ent;
+	}
+	
+	@Override
+	public Object getChild(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getChildId(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getChildView(int group, int arg1, boolean arg2, View arg3,
+			ViewGroup arg4) {
+		// TODO Auto-generated method stub
+		//t3 is going to hold the image and the comments
+		LinearLayout t3 = new LinearLayout(context);
+		//going to be the comments of the user
+		TextView t1 = new TextView(context);
+		//image of wine
+		ImageView t2 = new ImageView(context);
+		t1.setText(entries[group].comment);
+		t2.setImageResource(R.drawable.classicgary);
+		t3.addView(t2);
+		t3.addView(t1);
+		return t3;
+	}
+
+	@Override
+	public int getChildrenCount(int arg0) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public Object getGroup(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getGroupCount() {
+		// TODO Auto-generated method stub
+		return entries.length;
+	}
+
+	@Override
+	public long getGroupId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getGroupView(int group, boolean arg1, View arg2, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		//t1 is going to appear first in the list
+		TextView title = new TextView(context);
+		title.setText("Title: "+entries[group].wine.name);
+		return title;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isChildSelectable(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- Android/src/com/yoloswag/vino/ViewLogEntryFragment.java
+++ Android/src/com/yoloswag/vino/ViewLogEntryFragment.java
@@ -1,10 +1,13 @@
 package com.yoloswag.vino;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.*;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class ViewLogEntryFragment extends Fragment {
@@ -18,13 +21,11 @@ public class ViewLogEntryFragment extends Fragment {
 	 {
 	   super.onCreate(savedInstanceState);
 	   entries = Entry.getAll();
-	   
      }
 	
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_view_entry, container, false);
-        //ListView dummyTextView = (ListView) rootView.findViewById(R.id.section_label);
         
         //creates seperate view if there no entries
         //rootView1 is the view that would be displayed incase if there are no entries
@@ -33,7 +34,9 @@ public class ViewLogEntryFragment extends Fragment {
         	 View rootView1 = inflater.inflate(R.layout.fragment_view_no_entry, container, false);
         	 return rootView1;
         }
-        
+ 	   ExpandableListView list;
+ 	   list = (ExpandableListView) rootView.findViewById(R.id.viewLogEx);
+ 	   list.setAdapter(new ViewLogEntryAdapter(this.getActivity(), entries));
         
         return rootView;
     }
