fa6158d3a5eeaea6c5489b520c9cf545ee64d02a
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-05-10 15:11:29 -0700

    Pseudocode for sorting by ratings

--- Android/src/com/yoloswag/vino/FavoritesAdapter.java
+++ Android/src/com/yoloswag/vino/FavoritesAdapter.java
@@ -16,14 +16,36 @@ public class FavoritesAdapter extends BaseExpandableListAdapter
 	private Context context;
 	private FavoritesFragment favoritesFragment;
 	
+	Entry favoriteWines[] = Entry.getAll();
+	
+	//private sortedRatings()
+	
+	
+	/*procedure bubbleSort( A : list of sortable items )
+    n = length(A)
+    repeat
+       newn = 0
+       for i = 1 to n-1 inclusive do
+          if A[i-1] > A[i] then
+             swap(A[i-1], A[i])
+             newn = i
+          end if
+       end for
+       n = newn
+    until n = 0
+end procedure*/
+	
+	/*
 	//Array of favorite wines list to display 
 	String favoriteWines[]  = { "2008 YellowTail Muscato",  
 							    "2006 Montes Classic Cabernet Sauvignon", 
 							    "2011 Dr. Loosen Dr. L Riesling",
 							    "2010 Sutter Home White Zinfandel" };
+	*/
 	
 	//Temporary array of wine suggestions
 	String recommendationWines[][] = { {"Suggestion 1", "Suggestion 2", "Suggestion 3"},
+									   {"Suggestion 1", "Suggestion 2", "Suggestion 3"},
 									   {"Suggestion 1", "Suggestion 2", "Suggestion 3"},
 									   {"Suggestion 1", "Suggestion 2", "Suggestion 3"},
 									   {"Suggestion 1", "Suggestion 2", "Suggestion 3"} };
@@ -52,8 +74,9 @@ public class FavoritesAdapter extends BaseExpandableListAdapter
 	}
 
 	@Override
-	public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView,
-			ViewGroup parent) {
+	public View getChildView(int groupPosition, int childPosition,
+			boolean isLastChild, View convertView, ViewGroup parent) 
+	{
 		// TODO Auto-generated method stub
 		TextView textview = new TextView(context);
 		textview.setText(recommendationWines[groupPosition][childPosition]);
@@ -86,10 +109,12 @@ public class FavoritesAdapter extends BaseExpandableListAdapter
 	}
 
 	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+	public View getGroupView(int groupPosition, boolean isExpanded,
+			                 View convertView, ViewGroup parent) 
+	{
 		// TODO Auto-generated method stub
 		TextView textview = new TextView(context);
-		textview.setText(favoriteWines[groupPosition]);
+		textview.setText(favoriteWines[groupPosition].wine.name);
 		textview.setPadding(50, 20, 20, 20);
 		return textview;
 	}
