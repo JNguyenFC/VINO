51daf3f097319e61a3bae6c46db274ae6d065250
Max Takano <maxytakano@gmail.com>
2013-05-10 19:30:47 -0700

    adding camera to the new entry fragment just the preview

--- Android/src/com/yoloswag/vino/NewEntryFragment.java
+++ Android/src/com/yoloswag/vino/NewEntryFragment.java
@@ -45,7 +45,7 @@ public class NewEntryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) 
     {
-        View rootView = inflater.inflate(R.layout.fragment_new_entry, container, false);
+        View rootView = inflater.inflate(R.layout.camera, container, false);
         //TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
         //dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
         return rootView;
