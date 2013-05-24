4ecdabb4df8fc7c7775803ecb664ccb58784dd4e
Wes Tibbitts <wtibbitts@gmail.com>
2013-05-23 17:43:59 -0700

    Changed suggestions/recommendations to pull data from adapter, but only limited to one suggestion

--- Android/src/com/yoloswag/vino/favorites/SuggestionsAdapter.java
+++ Android/src/com/yoloswag/vino/favorites/SuggestionsAdapter.java
@@ -1,5 +1,53 @@
 package com.yoloswag.vino.favorites;
 
-public class SuggestionsAdapter {
+import java.util.List;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yoloswag.vino.R;
+
+public class SuggestionsAdapter extends ArrayAdapter<RowItem> {
+
+	Context context;
+	
+	public SuggestionsAdapter(SuggestionsFragment suggestionsFragment, int resourceId,
+            List<RowItem> items) {
+		super(suggestionsFragment.getActivity(), resourceId, items);
+		this.context = suggestionsFragment.getActivity();
+	}
+
+	private class ViewHolder{
+		ImageView imageView;
+		TextView txtName;
+		//TextView txtVarietal;
+		//TextView textVintage;
+	}
+	
+	public View getView(int position, View convertView, ViewGroup parent){
+		ViewHolder holder = null;
+		RowItem rowItem = getItem(position);
+		
+		LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		if(convertView == null)
+		{
+			convertView = mInflater.inflate(R.layout.suggestions, null);
+			holder = new ViewHolder();
+			holder.txtName = (TextView) convertView.findViewById(R.id.name);
+			holder.imageView = (ImageView) convertView.findViewById(R.id.icon);
+			convertView.setTag(holder);
+		}
+		else
+			holder = (ViewHolder) convertView.getTag();
+		holder.txtName.setText(rowItem.getName());
+		holder.imageView.setImageResource(rowItem.getImageID());
+		
+		return convertView;
+	}
 }
--- Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
+++ Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
@@ -1,23 +1,48 @@
 package com.yoloswag.vino.favorites;
 
-import com.yoloswag.vino.R;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+
+import com.yoloswag.vino.R;
+import com.yoloswag.vino.favorites.*;
 
 public class SuggestionsFragment extends Fragment {
 
+	 public static final String[] names = new String[] {"Wine1", "Wine2", "Wine3", "Wine4"};
+	 
+	 public static final Integer[] images = new Integer[] {R.drawable.wineicon, R.drawable.wine,
+		                                                   R.drawable.sauvignon_blanc,
+		                                                   R.drawable.classicgary};
+	 
+	 ListView listView;
+	 List<RowItem> rowItems;
+	
 	 @Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
-	        View rootView = inflater.inflate(R.layout.suggestions, container, false);
-	        //need to set adapter (?)
+		
+
 	        
-	        return rootView;
+		 rowItems = new ArrayList<RowItem>();
+		 for (int i = 0; i < names.length; ++i)
+		 {
+			 RowItem item = new RowItem(images[i], names[i]);
+			 rowItems.add(item);
+		 }
+		 
+		 View rootView = inflater.inflate(R.layout.suggestions_list, container, false);
+		 listView = (ListView)rootView.findViewById(R.id.list);
+		 listView.setAdapter(new SuggestionsAdapter(this, R.layout.suggestions, rowItems));
+	        
+	     return rootView;
 	    }
 	 
 }
