9ee016235dfdf25b7ede3ea3ccc6220c8b7a6aba
Alex Tsang <me@a13xt5ang.com>
2013-05-28 18:15:43 -0700

    Add examples of how to add wines to the db

--- Android/src/com/yoloswag/vino/db/DatabaseHelper.java
+++ Android/src/com/yoloswag/vino/db/DatabaseHelper.java
@@ -16,91 +16,119 @@ import com.yoloswag.vino.model.Entry;
 import com.yoloswag.vino.model.Wine;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
-    // name of the database file for your application -- change to something appropriate for your app
-    private static final String DATABASE_NAME = "VxIxNxOdDxB.sqlite";
+	// name of the database file for your application -- change to something appropriate for your app
+	private static final String DATABASE_NAME = "VxIxNxOdDxB.sqlite";
 
-    // any time you make changes to your database objects, you may have to increase the database version
-    private static final int DATABASE_VERSION = 1;
+	// any time you make changes to your database objects, you may have to increase the database version
+	private static final int DATABASE_VERSION = 1;
 
-    // the DAO object we use to access the SimpleData table
-    private Dao<Entry, Integer> entryDao = null;
-    private Dao<Wine, Integer> wineDao = null;
+	// the DAO object we use to access the SimpleData table
+	private Dao<Entry, Integer> entryDao = null;
+	private Dao<Wine, Integer> wineDao = null;
 
-    public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
 
-    @Override
-    public void onCreate(SQLiteDatabase database,ConnectionSource connectionSource) {
-        try {
-            TableUtils.createTable(connectionSource, Entry.class);
-            TableUtils.createTable(connectionSource, Wine.class);
-        } catch (SQLException e) {
-            Log.e(DatabaseHelper.class.getName(), "Can't create database", e);
-            throw new RuntimeException(e);
-        } catch (java.sql.SQLException e) {
-            e.printStackTrace();
-        }
-        //Wine a = new Wine("Castello Banfi", "Italy", "2008", 12.9, "red", "Chianti", "dry");
-        //new Wine("Chardonnay", "USA","2013",3,"white","Barefoot","dry").save();
-        //a.save();
-    	Entry b = new Entry(Wine.a, "title1", "France", "yoloswaging it up", 3);
-    	b.save();
-    	Entry.a.save();
-    	Entry.b.save();
-    	Entry.c.save();
-    	Entry.d.save();
-    	Entry.e.save();
-    	Entry e = new Entry(Wine.c, "title2", "New Zealand", "hi", 1);
-    	e.save();
-    	Entry d = new Entry(Wine.d, "title3", "California", "WHISTLE GOES WOO WOO", 5);
-    	d.save();
-    	Entry c = new Entry(Wine.a, "title4", "France", "If you see this it doesn't work ]:", 3);
-    	c.save();
-    	c.comment = "If you see this then it works [:";
-    	c.save();
-    }
+	@Override
+	public void onCreate(SQLiteDatabase database,ConnectionSource connectionSource) {
+		try {
+			TableUtils.createTable(connectionSource, Entry.class);
+			TableUtils.createTable(connectionSource, Wine.class);
+		} catch (SQLException e) {
+			Log.e(DatabaseHelper.class.getName(), "Can't create database", e);
+			throw new RuntimeException(e);
+		} catch (java.sql.SQLException e) {
+			e.printStackTrace();
+		}
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db,ConnectionSource connectionSource, int oldVersion, int newVersion) {
-        try {
-            List<String> allSql = new ArrayList<String>(); 
-            switch(oldVersion) 
-            {
-              case 1: 
-                  //allSql.add("alter table AdData add column `new_col` VARCHAR");
-                  //allSql.add("alter table AdData add column `new_col2` VARCHAR");
-            }
-            for (String sql : allSql) {
-                db.execSQL(sql);
-            }
-        } catch (SQLException e) {
-            Log.e(DatabaseHelper.class.getName(), "exception during onUpgrade", e);
-            throw new RuntimeException(e);
-        }
-        
-    }
+		{
+			// Here is your example @Jasmine
+			// (Some of these details are made up)
+			// The wine we are inserting for this example is:
+			// Name				: Barefoot Chardonnay
+			// Region			: USA
+			// Vintage			: 1940
+			// Category			: White
+			// Varietal			: Chardonnay
+			// Sweet/Dry		: Dry
+			// Producer/Winery	: Barefoot
 
-    public Dao<Entry, Integer> getEntryDao() {
-        if (null == entryDao) {
-            try {
-                entryDao = getDao(Entry.class);
-            }catch (java.sql.SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return entryDao;
-    }
+			String name = "Barefoot Chardonnay";
+			String region = "USA";
+			String vintage = "1940";
+			String category = "White";
+			String varietal = "Chardonnay";
+			String sweetdry = "Dry";
+			String producer = "Barefoot";
 
-    public Dao<Wine, Integer> getWineDao() {
-        if (null == wineDao) {
-            try {
-                wineDao = getDao(Wine.class);
-            }catch (java.sql.SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return wineDao;
-    }
+			// Create the wine
+			Wine example = new Wine(name, region, vintage, category, varietal, sweetdry, producer);
+
+			// Save the wine to the db
+			example.save();
+		}
+
+		//Wine a = new Wine("Castello Banfi", "Italy", "2008", 12.9, "red", "Chianti", "dry");
+		//new Wine("Chardonnay", "USA","2013",3,"white","Barefoot","dry").save();
+		//a.save();
+		Entry b = new Entry(Wine.a, "title1", "France", "yoloswaging it up", 3);
+		b.save();
+		Entry.a.save();
+		Entry.b.save();
+		Entry.c.save();
+		Entry.d.save();
+		Entry.e.save();
+		Entry e = new Entry(Wine.c, "title2", "New Zealand", "hi", 1);
+		e.save();
+		Entry d = new Entry(Wine.d, "title3", "California", "WHISTLE GOES WOO WOO", 5);
+		d.save();
+		Entry c = new Entry(Wine.a, "title4", "France", "If you see this it doesn't work ]:", 3);
+		c.save();
+		c.comment = "If you see this then it works [:";
+		c.save();
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db,ConnectionSource connectionSource, int oldVersion, int newVersion) {
+		try {
+			List<String> allSql = new ArrayList<String>(); 
+			switch(oldVersion) 
+			{
+			case 1: 
+				//allSql.add("alter table AdData add column `new_col` VARCHAR");
+				//allSql.add("alter table AdData add column `new_col2` VARCHAR");
+			}
+			for (String sql : allSql) {
+				db.execSQL(sql);
+			}
+		} catch (SQLException e) {
+			Log.e(DatabaseHelper.class.getName(), "exception during onUpgrade", e);
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public Dao<Entry, Integer> getEntryDao() {
+		if (null == entryDao) {
+			try {
+				entryDao = getDao(Entry.class);
+			}catch (java.sql.SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return entryDao;
+	}
+
+	public Dao<Wine, Integer> getWineDao() {
+		if (null == wineDao) {
+			try {
+				wineDao = getDao(Wine.class);
+			}catch (java.sql.SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return wineDao;
+	}
 
 }
