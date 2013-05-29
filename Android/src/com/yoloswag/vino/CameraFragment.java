fd05b771628f4f67b9f97f00ba87977929b31ec9
tiffanykyi@gmail.com <tiffanykyi@gmail.com>
2013-05-29 00:51:24 -0700

    Commenting out extra URI line in CameraFragment

--- Android/src/com/yoloswag/vino/CameraFragment.java
+++ Android/src/com/yoloswag/vino/CameraFragment.java
@@ -147,7 +147,7 @@ public class CameraFragment extends Fragment {
 			@Override
 			public void onClick(View arg1) {
 				//save picture
-				String uri = Util.getOutputMediaFileUri().toString();// Getting URI
+				//String uri = Util.getOutputMediaFileUri().toString();// Getting URI
 				//Entry e = 
 				Environment.getExternalStorageState();// Checking that SDCard exists
 				File file = Util.getOutputMediaFile();
