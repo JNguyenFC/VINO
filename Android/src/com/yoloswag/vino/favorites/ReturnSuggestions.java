bfd5ef0fc0be256b2ec4f12fc54b5d0d24f48cbd
Wes Tibbitts <wtibbitts@gmail.com>
2013-05-30 22:00:37 -0700

    Beginning to build suggestions helper method

---
+++ Android/src/com/yoloswag/vino/favorites/ReturnSuggestions.java
@@ -0,0 +1,16 @@
+package com.yoloswag.vino.favorites;
+
+import com.yoloswag.vino.model.Wine;
+
+public class ReturnSuggestions {
+	/*
+	Wine[] suggestionList = null;
+	
+	Wine[] wines = Wine.getAll();
+	
+	public Wine[] suggestions()
+	{
+		return 
+	}
+*/
+}
