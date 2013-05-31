b99f87ff2ec772955305cc61acc477b1c6f1f13b
Wes Tibbitts <wtibbitts@gmail.com>
2013-05-30 17:54:34 -0700

    changed rating to double from int so we can use halfs

--- Android/src/com/yoloswag/vino/model/Entry.java
+++ Android/src/com/yoloswag/vino/model/Entry.java
@@ -32,7 +32,7 @@ public class Entry
 	@DatabaseField
 	public String region;
 	@DatabaseField
-	public int rating;
+	public double rating;
 	//@DatabaseField
 	//protected Image image;
 	//protected Image image;
@@ -45,18 +45,18 @@ public class Entry
 	
 	/**  Fake database of Entry (to be deleted later)
 	 */
-	public static Entry a = new Entry(Wine.a, "fuck you max", "France", "To all the ladies in the place with style and grace, allow me to lace these lyrical douches in your bushes. Who rock grooves and make moves with all the mommies? The back of the club, sippin' Moet, is where you'll find me. The back of the club, mackin' hoes, my crew's behind me; mad question askin', blunt passin', music blastin' but I just can't quit.", 3);
-	public static Entry b = new Entry(Wine.b, "#drank", "US", "gary", 5);
-	public static Entry c = new Entry(Wine.c, "#yolo", "Canada", "i love justin timberlake", 1);
-	public static Entry d = new Entry(Wine.d, "suq madiq", "Germany",  "derp", 4);
-	public static Entry e = new Entry(Wine.e, "liqa madiq", "Italy", "trolls", 2);
+	public static Entry a = new Entry(Wine.a, "fuck you max", "France", "To all the ladies in the place with style and grace, allow me to lace these lyrical douches in your bushes. Who rock grooves and make moves with all the mommies? The back of the club, sippin' Moet, is where you'll find me. The back of the club, mackin' hoes, my crew's behind me; mad question askin', blunt passin', music blastin' but I just can't quit.", 5);
+	public static Entry b = new Entry(Wine.b, "#drank", "US", "gary", 3.5);
+	public static Entry c = new Entry(Wine.c, "#yolo", "Canada", "i love justin timberlake", 2.5);
+	public static Entry d = new Entry(Wine.d, "suq madiq", "Germany",  "derp", 1.5);
+	public static Entry e = new Entry(Wine.e, "liqa madiq", "Italy", "trolls", 1);
 	
 	public Entry() {
 		
 	}
 	
 	//constructor for fake database
-	public Entry(Wine wine, String title, String location, String comment, int rating) {
+	public Entry(Wine wine, String title, String location, String comment, double rating) {
 		this.wine = Wine.get(wine);
 		this.location = location;
 		this.comment = comment;
