24c4c3e8a698a51ed1ffaceb8ddc6b04692c42d3
Wes Tibbitts <wtibbitts@gmail.com>
2013-06-01 11:46:52 -0700

    Continued work on Return Suggestions

--- Android/src/com/yoloswag/vino/favorites/ReturnSuggestions.java
+++ Android/src/com/yoloswag/vino/favorites/ReturnSuggestions.java
@@ -3,14 +3,26 @@ package com.yoloswag.vino.favorites;
 import com.yoloswag.vino.model.Wine;
 
 public class ReturnSuggestions {
-	/*
-	Wine[] suggestionList = null;
+	
+	Wine[] suggestionsList = null;
 	
 	Wine[] wines = Wine.getAll();
 	
 	public Wine[] suggestions()
 	{
-		return 
+		int n = wines.length;
+		
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j)
+				{
+					if (i != j)
+					{
+						if (wines[i].category.category.compareToIgnoreCase(wines[j].category.category) != 0);
+					}
+				}
+			}
+		return suggestionsList;
 	}
-*/
+
 }
