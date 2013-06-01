1b57848dc5cff970392a346519634234fe3b1920
Wes Tibbitts <wtibbitts@gmail.com>
2013-06-01 14:33:30 -0700

    Fixed Suggestions

--- Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
+++ Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
@@ -19,7 +19,7 @@ public class SuggestionsFragment extends Fragment
 	 //public boolean OPEN = false;
 	 
 
-	 public static final Wine[][] names = new Wine [FavoritesAdapter.favoriteSize][4];
+	 
 			                           /* new Wine[][] { {Wine.a, Wine.b, Wine.c, Wine.d},
 		 											     {Wine.d, Wine.c, Wine.b, Wine.a},
 	 												     {Wine.c, Wine.d, Wine.a, Wine.b} };
@@ -39,10 +39,7 @@ public class SuggestionsFragment extends Fragment
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
 		
-		 for (int i = 0; i < FavoritesAdapter.favoriteSize; ++i)
-		 {
-			 names[i] = suggestions(FavoritesAdapter.favoriteWines[i]);
-		 }
+
 		
 	        
 		 rowItems = new ArrayList<RowItem>();
@@ -62,49 +59,6 @@ System.out.println("AFTER SETADAPTER");
 	     return rootView;
 	    }
 	 
-		List<Wine> suggestionsList = new ArrayList<Wine>();
-		
-		Wine[] wines = Wine.getAll();
-		
-		public Wine[] suggestions(Wine clickedFavorite)
-		{
-			int n = wines.length;
-			int counter = 0;
 
-					for (int j = 0; j < n; ++j)
-					{
-						if (clickedFavorite != wines[j] && wines[j].rating == 0)
-						{
-							if ((clickedFavorite.category.category.compareToIgnoreCase(wines[j].category.category) == 0)
-								&& (clickedFavorite.sweetOrDry.taste.compareToIgnoreCase(wines[j].sweetOrDry.taste) == 0)
-							    && (clickedFavorite.varietal.varietal_name.compareToIgnoreCase(wines[j].varietal.varietal_name) != 0))
-							{
-							             suggestionsList.add(wines[j]);
-							             ++counter;
-							}
-							
-							             
-							else if((clickedFavorite.category.category.compareToIgnoreCase(wines[j].category.category) == 0)
-							       && (clickedFavorite.sweetOrDry.taste.compareToIgnoreCase(wines[j].sweetOrDry.taste) == 0)
-							       && (counter < 4))
-							{
-								         suggestionsList.add(wines[j]);
-								         ++counter;
-							}
-							    
-						}
-					}
-				
-			Wine[] tempArray = suggestionsList.toArray(new Wine[suggestionsList.size()]);
-			
-			Wine[] suggestedWines = new Wine[4];
-			
-			for(int i = 0; i < 4; ++i)
-			{
-				suggestedWines[i] = tempArray[(int)(Math.random()*suggestionsList.size())];
-			}
-			
-			return suggestedWines;
-		}
 	 
 }
