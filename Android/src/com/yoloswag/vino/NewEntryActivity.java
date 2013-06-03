c5ffc9966200e89b8e11c7b16074fba71f44fc8f
tiffanykyi@gmail.com <tiffanykyi@gmail.com>
2013-06-02 22:14:00 -0700

    Commenting in NewEntryActivity

--- Android/src/com/yoloswag/vino/NewEntryActivity.java
+++ Android/src/com/yoloswag/vino/NewEntryActivity.java
@@ -27,6 +27,7 @@ import android.widget.Toast;
 
 public class NewEntryActivity extends Activity implements TextWatcher {
 	
+	// Autocomplete arrays
     AutoCompleteTextView myAutoComplete;
     AutoCompleteTextView myAutoComplete2;
     AutoCompleteTextView myAutoComplete3;
@@ -76,7 +77,6 @@ public class NewEntryActivity extends Activity implements TextWatcher {
 			ImageView imageView = (ImageView)this.findViewById(R.id.image);
 			imageView.setImageBitmap(bitmap);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}        
 		
@@ -164,7 +164,7 @@ public class NewEntryActivity extends Activity implements TextWatcher {
         b.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg1) {
-				// TODO Auto-generated method stub
+				// Creating entry
 				Entry e = new Entry();
 				EditText title = (EditText)findViewById(R.id.title);
 				EditText category = (EditText)findViewById(R.id.category);
@@ -221,8 +221,6 @@ public class NewEntryActivity extends Activity implements TextWatcher {
 					Toast.makeText(NewEntryActivity.this, "That year hasn't occurred yet!", Toast.LENGTH_SHORT).show();
 					return;
 				}
-				
-				//Toast.makeText(getActivity(), e.title, Toast.LENGTH_SHORT).show();
 
 				// Entry details
 				e.wine = new Wine("", "", "", "", "", "");
@@ -245,8 +243,6 @@ public class NewEntryActivity extends Activity implements TextWatcher {
 				finish();
 			}
         });
-
-		//DatabaseManager.init(this);
 	}
 	
 	
@@ -292,33 +288,19 @@ public class NewEntryActivity extends Activity implements TextWatcher {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-/*        // The activity is about to be destroyed.
-        // Calling ViewLogEntryFragment
-        Fragment fragment = new ViewLogEntryFragment();
-		FragmentTransaction transaction = fragment.getFragmentManager().beginTransaction();
-
-		transaction.replace(R.id.fragment_view_log_entry, fragment);
-		transaction.addToBackStack(null);
-		transaction.commit();*/
     }
     
     
     @Override
     public void afterTextChanged(Editable arg0) {
-     // TODO Auto-generated method stub
-
     }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count,
       int after) {
-     // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-     // TODO Auto-generated method stub
-
     }
 }
