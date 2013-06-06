e1f16c7d7034ed5857424ae05666ca8ff431c5a6
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-06-06 13:54:25 -0700

    Deleted unused file, added comments

--- Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
+++ Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
@@ -1,7 +1,12 @@
-package com.yoloswag.vino.favorites;
+/**
+ * Filename:    SuggestionsFragment.java
+ * Team:		VINO
+ * Description: Displays the ListView that holds the list of wine
+ *              recommendations. Sets the adapter for this ListView.
+ * Date:        8 Jun 2013
+ **/
 
-import java.util.ArrayList;
-import java.util.List;
+package com.yoloswag.vino.favorites;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,49 +16,31 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.yoloswag.vino.R;
-import com.yoloswag.vino.model.Wine;
 
+/** Inflates the View which holds the ListView of wine recommendations and
+ *  also sets the adapter for it.
+ */
 public class SuggestionsFragment extends Fragment
-{
-	
-	 //public boolean OPEN = false;
-	 
-
-	 
-			                           /* new Wine[][] { {Wine.a, Wine.b, Wine.c, Wine.d},
-		 											     {Wine.d, Wine.c, Wine.b, Wine.a},
-	 												     {Wine.c, Wine.d, Wine.a, Wine.b} };
-	 /*public static final Wine[] names2 = new Wine[] {Wine.d, Wine.c, Wine.b, Wine.a};
-	 public static final Wine[] names3 = new Wine[] {Wine.c, Wine.d, Wine.a, Wine.b};*/
-	 
-	 public static final Integer[] images = new Integer[] {R.drawable.wine_vector, R.drawable.wine_vector,
+{	 
+	 public static final Integer[] images = new Integer[] {R.drawable.wine_vector, 
+		 												   R.drawable.wine_vector,
 		                                                   R.drawable.wine_vector,
 		                                                   R.drawable.wine_vector};
 	 
-	 //public static Wine[] names = null;
-	 
 	 ListView listView;
-	 List<RowItem> rowItems;
 	 
+	 /** Creates and returns the ListView of wines recommendations
+	  */
 	 @Override
-	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	            Bundle savedInstanceState) {
-		        
-		 rowItems = new ArrayList<RowItem>();
-
-		 
-		 for (int i = 0; i < 4; ++i)
-		 {
-			 RowItem item = new RowItem();
-			 rowItems.add(item);
-		 }
-		 
+	 public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) 
+	 {
 		 View rootView = inflater.inflate(R.layout.suggestions_list, container, false);
 		 listView = (ListView)rootView.findViewById(R.id.list);
-		 listView.setAdapter(new SuggestionsAdapter(this, R.layout.suggestions, rowItems));
+		 listView.setAdapter(new SuggestionsAdapter(this, R.layout.suggestions));
 		 
 	     return rootView;
-	    }
+	 }
 	 
 
 	 
