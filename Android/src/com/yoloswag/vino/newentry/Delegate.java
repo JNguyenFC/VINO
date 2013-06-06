a7ff23e22f8c8f1aba1d93bbbb7610e35e91fd99
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-06-06 14:56:04 -0700

    Delegate.java -- Added comments

--- Android/src/com/yoloswag/vino/newentry/Delegate.java
+++ Android/src/com/yoloswag/vino/newentry/Delegate.java
@@ -1,9 +1,17 @@
+/**
+ * Filename:    Delegate.java
+ * Team:		VINO
+ * Description: 
+ * Date:        8 Jun 2013
+ **/
+
 package com.yoloswag.vino.newentry;
 
-public class Delegate {
-
-	// Go back to the news feed after saving entry
-	public void finished() {
-		
+public class Delegate 
+{
+	/** Returns to entries feed after submitting a new entry
+	 */
+	public void finished() 
+	{	
 	}
 }
