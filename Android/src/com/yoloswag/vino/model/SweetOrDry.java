7358caf7e9289d4c2000a28d253bdfdd1528c1ae
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-06-06 14:48:37 -0700

    SweetOrDry.java -- Added comments, removed getAll()

--- Android/src/com/yoloswag/vino/model/SweetOrDry.java
+++ Android/src/com/yoloswag/vino/model/SweetOrDry.java
@@ -1,24 +1,22 @@
+/**
+ * Filename:    SweetOrDry.java
+ * Team:		VINO
+ * Description: 
+ * Date:        8 Jun 2013
+ **/
+
 package com.yoloswag.vino.model;
 
 import java.io.Serializable;
 
-public class SweetOrDry implements Serializable {
-	/**
-	 * 
-	 */
+public class SweetOrDry implements Serializable 
+{
 	private static final long serialVersionUID = 1L;
 	public String taste;
 
-	public SweetOrDry(String taste){
+	// Initializes the String taste in SweetOrDry with the parameter
+	public SweetOrDry(String taste)
+	{
 		this.taste = taste;		
-	}
-
-	//returns a SweetOrDry array containing arbitrary data
-	//so we can begin implementing our other methods
-	public static SweetOrDry[] getAll() {
-		return  new SweetOrDry[] { 
-				new SweetOrDry("SWEET"), 
-				new SweetOrDry("DRY")
-		};
 	}
 }
