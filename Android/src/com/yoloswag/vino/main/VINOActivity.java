a6e7dfb7e182ebf62efbe34d5ec8841caa9a9c9a
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-06-06 14:28:41 -0700

    Added comments and removed the dummy fragment

--- Android/src/com/yoloswag/vino/main/VINOActivity.java
+++ Android/src/com/yoloswag/vino/main/VINOActivity.java
@@ -1,13 +1,13 @@
+/**
+ * Filename:    VINOActivity.java
+ * Team:		VINO
+ * Description: The driver for our application.
+ * Date:        8 Jun 2013
+ **/
+
 package com.yoloswag.vino.main;
 
 import java.util.Locale;
-
-import com.yoloswag.vino.CameraFragment;
-import com.yoloswag.vino.R;
-import com.yoloswag.vino.db.DatabaseManager;
-import com.yoloswag.vino.favorites.FavoritesFragment;
-import com.yoloswag.vino.viewentries.DiaryFragment;
-
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
@@ -21,73 +21,67 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class VINOActivity extends FragmentActivity implements ActionBar.TabListener {
+import com.yoloswag.vino.CameraFragment;
+import com.yoloswag.vino.R;
+import com.yoloswag.vino.db.DatabaseManager;
+import com.yoloswag.vino.favorites.FavoritesFragment;
+import com.yoloswag.vino.viewentries.DiaryFragment;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-     * will keep every loaded fragment in memory. If this becomes too memory
-     * intensive, it may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+public class VINOActivity extends FragmentActivity implements ActionBar.TabListener 
+{
+
+    /** The PagerAdapter that will provide fragments for each of the tabs. A
+     *  FragmentPagerAdapter derivative is used, which will keep every loaded
+     *  fragment in memory.
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
  
     
-    /**
-     * The {@link ViewPager} that will host the section contents.
+    /** The ViewPager that will host the section contents.
      */
     ViewPager mViewPager;
 
-    @SuppressWarnings("deprecation")
-	@SuppressLint("NewApi")
+    @SuppressLint("NewApi")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vino);
         DatabaseManager.init(this);
 
-        // Set up the action bar.
+        // Sets up the tabs
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         // Create the adapter that will return a fragment for each of the three
-        // primary sections of the app.
+        // tabs
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
+        // Set up the ViewPager with the sections adapter
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+        // Allow swiping between different sections
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() 
+        {
             @Override
-            public void onPageSelected(int position) {
+            public void onPageSelected(int position) 
+            {
                 actionBar.setSelectedNavigationItem(position);
             }
         });
 
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+        // Adds 3 tabs
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) 
+        {
             // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
+            // the adapter
             actionBar.addTab(
                     actionBar.newTab()
                             .setText(mSectionsPagerAdapter.getPageTitle(i))
                             .setTabListener(this));
         }
-        //actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
-        //actionBar.setStackedBackgroundDrawable(new ColorDrawable(Color.parseColor("#822b4f")));
         actionBar.setSplitBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
     }
 /*
@@ -98,79 +92,72 @@ public class VINOActivity extends FragmentActivity implements ActionBar.TabListe
         return true;
     }*/
     
+    /** Switch to corresponding page in ViewPager when the given tab is selected
+     */
     @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) 
+    {
         mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) 
+    {
     }
 
     @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) 
+    {
     }
     
-    // When the submit button in NewEntry is clicked
-    public void onSubmit() {
-        // When the submit button is selected, switch to the corresponding page in
-        // the ViewPager.
+    /** When Submit button in NewEntryActivity is clicked, switch to
+     *  the corresponding page in the ViewPager
+     */
+    public void onSubmit() 
+    {
         mViewPager.setCurrentItem(0);
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
+    /** A FragmentPagerAdapter that returns a fragment corresponding to
+     *  the specified tab
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter 
+    {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        public SectionsPagerAdapter(FragmentManager fm) 
+        {
             super(fm);
         }
 
+        /** Instantiates fragment for the given page
+         */
         @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a DummySectionFragment (defined as a static inner class
-            // below) with the page number as its lone argument.
-            Fragment fragment = new DummySectionFragment();
-            Bundle args = new Bundle();
-            
-            // If Favorites tab is clicked, show favorites Fragment
-            if (position == 2)
+        public Fragment getItem(int position) 
+        {   
+            switch (position)
             {
-                // Instantiate Fragment for favorites
-                Fragment favoritesFragment = new FavoritesFragment();
-                return favoritesFragment;
-            }
-            if(position == 1)
-            {
-            	//return new NewEntryFragment(); using the camera for nao
-            	return new CameraFragment();
-            }
-             //this is for the view log, first tab   
-            if (position == 0) 
-            {
-            	//Fragment re1 = new ViewLogEntryFragment();
-            	//return (Fragment) re1;
-            	return new DiaryFragment();
+            	case 0: return new DiaryFragment();
+            	case 1: return new CameraFragment();
+            	case 2: return new FavoritesFragment();
             }
             
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-            fragment.setArguments(args);
-            return fragment;
+            return new DiaryFragment();
+
         }
 
+        /** Gets total count of tabs (3)
+         */
         @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3; // for now
+        public int getCount() 
+        {
+            return 3;
         }
 
+        /** Gets label for given tab
+         */
         @Override
-        public CharSequence getPageTitle(int position) {
+        public CharSequence getPageTitle(int position) 
+        {
             Locale l = Locale.getDefault();
             switch (position) {
                 case 0:
@@ -184,37 +171,12 @@ public class VINOActivity extends FragmentActivity implements ActionBar.TabListe
         }
     }
 	
-	@SuppressWarnings("deprecation")
+    /** Called when the Activity exits
+     */
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
+	public void onActivityResult(int requestCode, int resultCode, Intent data) 
+	{
 		super.onActivityResult(requestCode, resultCode, data);
-		System.out.println("ASBKJDKSAD");
 		onSubmit();
 	}
-
-    /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_vino_dummy, container, false);
-            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
 }
