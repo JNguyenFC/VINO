6f858a2287f52e11c75ded49a92d1ebd2d18769d
Wes Tibbitts <wtibbitts@gmail.com>
2013-06-06 16:33:08 -0700

    Moved functionality in Fragment to new class ReturnSuggestions to fit MVC

--- Android/src/com/yoloswag/vino/favorites/FavoritesFragment.java
+++ Android/src/com/yoloswag/vino/favorites/FavoritesFragment.java
@@ -7,8 +7,6 @@
 
 package com.yoloswag.vino.favorites;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -28,6 +26,7 @@ public class FavoritesFragment extends Fragment implements OnGroupExpandListener
 	ExpandableListView exv;
 	public static Wine[][] suggestionNames = new Wine [FavoritesAdapter.favoriteSize][4];
 	private FavoritesAdapter adapter;
+	public static ReturnSuggestions sList = new ReturnSuggestions();
 
 	// Default constructor
 	public FavoritesFragment() 
@@ -49,7 +48,7 @@ public class FavoritesFragment extends Fragment implements OnGroupExpandListener
 
 		suggestionNames = new Wine [FavoritesAdapter.favoriteSize][4];
 		for (int i = 0; i < FavoritesAdapter.favoriteSize; ++i) 
-			suggestionNames[i] = suggestions(FavoritesAdapter.favoriteWines[i]);
+			suggestionNames[i] = sList.suggestions(FavoritesAdapter.favoriteWines[i]);
 	}
 
 	/** Creates and returns the ExpandableListView of favorite wines
@@ -58,14 +57,16 @@ public class FavoritesFragment extends Fragment implements OnGroupExpandListener
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 		   Bundle savedInstanceState) 
 	{
-		for (int i = 0; i < FavoritesAdapter.favoriteSize; ++i) 
-			suggestionNames[i] = suggestions(FavoritesAdapter.favoriteWines[i]);
+		
 
 		View rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
 		exv = (ExpandableListView)rootView.findViewById(R.id.expandableListView1);
 		exv.setOnGroupExpandListener((OnGroupExpandListener) this);
 		adapter = new FavoritesAdapter(this);
 		exv.setAdapter(adapter);
+		
+		for (int i = 0; i < FavoritesAdapter.favoriteSize; ++i) 
+			suggestionNames[i] = sList.suggestions(FavoritesAdapter.favoriteWines[i]);
 
 		return rootView;
 	}
@@ -86,90 +87,5 @@ public class FavoritesFragment extends Fragment implements OnGroupExpandListener
 		}
 	}
 
-	//TODO: probably move this out of the fragment lol?
-	/** Builds list of recommendations for the favorite wine that the user selects
-	 */
-	public Wine[] suggestions(Wine clickedFavorite)
-	{
-		List<Wine> suggestionsList = new ArrayList<Wine>();
-		Wine[] wines = Wine.getAll();
 
-		// Ensures that there are at least 4 wines in the list of recommendations
-		int counter = 0;
-
-		// Loops through all of the wines in the database, only recommending
-		// wines that the user has not yet tasted
-		for (int j = 0; j < wines.length; ++j)
-		{
-			if (clickedFavorite != wines[j] && wines[j].rating == 0)
-			{
-				// Gives priority recommendations based on wine category, taste, and varietal; second priority
-				// recommendations go to wines based on category and taste
-				if ((clickedFavorite.category.category.compareToIgnoreCase(wines[j].category.category) == 0)
-						&& (clickedFavorite.sweetOrDry.taste.compareToIgnoreCase(wines[j].sweetOrDry.taste) == 0)
-						&& (clickedFavorite.varietal.varietal_name.compareToIgnoreCase(wines[j].varietal.varietal_name) == 0))
-				{						             
-					suggestionsList.add(wines[j]);
-					++counter;
-				}
-
-				else if ((clickedFavorite.category.category.compareToIgnoreCase(wines[j].category.category) == 0)
-						&& (clickedFavorite.sweetOrDry.taste.compareToIgnoreCase(wines[j].sweetOrDry.taste) == 0))
-				{         
-					suggestionsList.add(wines[j]);
-					++counter;
-				}
-
-				// If 4 wine recommendations have yet to be curated and more than half of the wine database
-				// has already been traversed, start adding wines to the list of recommendations based on
-				// taste, then category, then varietal, then region; if none of those match, then add any
-				else if (counter < 4 && j > (wines.length/2))
-				{		
-					if ((clickedFavorite.sweetOrDry.taste.compareToIgnoreCase(wines[j].sweetOrDry.taste) == 0))
-					{
-						suggestionsList.add(wines[j]);
-						++counter;
-					}
-					
-					else if ((clickedFavorite.category.category.compareToIgnoreCase(wines[j].category.category) == 0))
-					{
-						suggestionsList.add(wines[j]);
-						++counter;
-					}
-					
-					else if ((clickedFavorite.varietal.varietal_name.compareToIgnoreCase(wines[j].varietal.varietal_name) == 0))
-					{
-						suggestionsList.add(wines[j]);
-						++counter;
-					}
-
-					else if ((clickedFavorite.region.region.compareToIgnoreCase(wines[j].region.region) == 0))
-					{
-						suggestionsList.add(wines[j]);
-						++counter;
-					}
-
-					else
-					{
-						suggestionsList.add(wines[j]);
-						++counter;
-					}
-
-				}
-
-			}
-		}
-
-		Wine[] suggestedWines = new Wine[4];
-
-		// Adds 4 recommendations randomly chosen from the list of recommendations
-		for (int i = 0; i < 4; ++i)
-		{
-			int pos = (int)(Math.random()*suggestionsList.size());
-			if (suggestionsList.size() > pos)
-				suggestedWines[i] = suggestionsList.remove(pos);			
-		}
-
-		return suggestedWines;
-	}
 }
