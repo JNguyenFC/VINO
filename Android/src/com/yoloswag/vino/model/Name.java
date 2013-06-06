e1a6125ee7e0bb305cfb2554f2f5bdc28372d1ea
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-06-06 14:45:41 -0700

    Removeod unused Image file, added comments and header to Name.java

--- Android/src/com/yoloswag/vino/model/Name.java
+++ Android/src/com/yoloswag/vino/model/Name.java
@@ -1,22 +1,22 @@
+/**
+ * Filename:    Name.java
+ * Team:		VINO
+ * Description: 
+ * Date:        8 Jun 2013
+ **/
+
 package com.yoloswag.vino.model;
 
 import java.io.Serializable;
 
-public class Name implements Serializable {
-
-	/**
-	 * 
-	 */
+public class Name implements Serializable 
+{
 	private static final long serialVersionUID = 1L;
 	public String producer;
 
+	// Initializes the String producer in Name with the parameter
 	public Name(String producer)
 	{
 		this.producer = producer;
 	}
-
-	public String getName() {
-		return "Generic Producer";
-	}
-
 }
