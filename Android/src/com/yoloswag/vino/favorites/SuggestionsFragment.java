b0411e99da86924aa8296276dead37c3a910291a
Jasmine Nguyen <jasmine.nguyen@mac.com>
2013-06-07 00:49:08 -0700

    SuggestionsFragment.java -- Added comments

--- Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
+++ Android/src/com/yoloswag/vino/favorites/SuggestionsFragment.java
@@ -1,3 +1,11 @@
+/**
+ * Filename:    SuggestionsFragment.java
+ * Team:		VINO
+ * Description: Displays the ListView that holds the list of wine
+ *              recommendations. Sets the adapter for this ListView.
+ * Date:        8 Jun 2013
+ **/
+
 package com.yoloswag.vino.favorites;
 
 import java.util.ArrayList;
@@ -11,34 +19,26 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.yoloswag.vino.R;
-import com.yoloswag.vino.model.Wine;
 
+/** Inflates the View which holds the ListView of wine recommendations and
+ *  also sets the adapter for it.
+ */
 public class SuggestionsFragment extends Fragment
-{
-
-	 //public boolean OPEN = false;
-
-
-
-			                           /* new Wine[][] { {Wine.a, Wine.b, Wine.c, Wine.d},
-		 											     {Wine.d, Wine.c, Wine.b, Wine.a},
-	 												     {Wine.c, Wine.d, Wine.a, Wine.b} };
-	 /*public static final Wine[] names2 = new Wine[] {Wine.d, Wine.c, Wine.b, Wine.a};
-	 public static final Wine[] names3 = new Wine[] {Wine.c, Wine.d, Wine.a, Wine.b};*/
-
-	 public static final Integer[] images = new Integer[] {R.drawable.wine_vector, R.drawable.wine_vector,
+{	 
+	 public static final Integer[] images = new Integer[] {R.drawable.wine_vector, 
+		 												   R.drawable.wine_vector,
 		                                                   R.drawable.wine_vector,
 		                                                   R.drawable.wine_vector};
-
-	 //public static Wine[] names = null;
 
 	 ListView listView;
 	 List<RowItem> rowItems;
 
+	 /** Creates and returns the ListView of wines recommendations
+	  */
 	 @Override
-	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	            Bundle savedInstanceState) {
-
+	 public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) 
+	 {
 		 rowItems = new ArrayList<RowItem>();
 
 
@@ -47,13 +47,13 @@ public class SuggestionsFragment extends Fragment
 			 RowItem item = new RowItem();
 			 rowItems.add(item);
 		 }
-
+		 
 		 View rootView = inflater.inflate(R.layout.suggestions_list, container, false);
 		 listView = (ListView)rootView.findViewById(R.id.list);
 		 listView.setAdapter(new SuggestionsAdapter(this, R.layout.suggestions, rowItems));
 
 	     return rootView;
-	    }
+	 }
 
 
 
